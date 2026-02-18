4d781bddddb018ab0ea90b91b7a021af777e5afd
ebarlowjr2 <ebarlowjr2@gmail.com>
2026-02-17 19:45:51 -0600

    Revert "Swap orb to Lottie animation"
    This reverts commit b928a7a8473a8ea19ad7d2ab2ae36f4cd8ccc539.

--- components/Orb.tsx
+++ components/Orb.tsx
@@ -1,7 +1,4 @@
-\"use client\";
-
-import React from \"react\";
-import \"@lottiefiles/dotlottie-wc\";
+import React from "react";
 
 type OrbState = "idle" | "listening" | "thinking" | "speaking" | "error";
 
@@ -20,14 +17,8 @@ const stateLabels: Record<OrbState, string> = {
 export default function Orb({ state }: OrbProps) {
   return (
     <div className="orb-wrap" aria-live="polite">
-      <div className={`orb-lottie ${state}`} role="img" aria-label={`Orb is ${state}`}>
-        <dotlottie-wc
-          src="https://lottie.host/e5f5b4e7-6f60-4cb1-819d-005ec90ae6bc/y3o3RMdJmt.lottie"
-          autoplay
-          loop
-          background="transparent"
-          speed="1"
-        />
+      <div className={`orb ${state}`} role="img" aria-label={`Orb is ${state}`}>
+        <div className="orb-glow" />
       </div>
       <div className="orb-state-label">{stateLabels[state]}</div>
     </div>
